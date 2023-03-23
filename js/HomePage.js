079a20a2cd6802fb537a536c2fad98cbece5cfe8
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-23 18:18:14 +0530

    [Jyoti Rao] Display Employee Details from Local Storage

--- js/HomePage.js
+++ js/HomePage.js
@@ -1,5 +1,5 @@
  /* UC5- Display Employee Details from JSON Object */
- window.addEventListener("DOMContentLoaded", (event) => {
+ /*window.addEventListener("DOMContentLoaded", (event) => {
     createInnerHtml();
  });
  const createInnerHtml=() => {
@@ -51,6 +51,18 @@
             _note: '',
             _id: new Date().getTime(),
             _profilePic: '../assets/E1.png'
+        },
+        {
+            _name: 'Anmol Rana',
+            _gender: 'male',
+            _department: [
+                'developer'
+            ],
+            _salary: '600000',
+            _startDate: '21 Feb 2022',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/E4.png'
         }
     ];
     return empPayrollListLocal;
@@ -62,4 +74,48 @@
         deptHtml=`${deptHtml}<div class='dept-label'>${dept}</div>`
      }
      return deptHtml;
-   }
+   }*/
+/* UC6- Display Employee Details from Local Storage*/
+
+let empPayrollList;
+window.addEventListener("DOMContentLoaded", (event) => {
+    empPayrollList = getEmployeePayrollDataStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+    localStorage.removeItem('editEmp');
+ });
+ const getEmployeePayrollDataStorage =() => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+ }
+
+ const createInnerHtml=() => {
+    const headerHtml="<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>StartDate</th><th>Actions</th>";
+   if(empPayrollList.length == 0) return;
+    let innerHtml=`${headerHtml}`;
+    for( const empPayrollData of empPayrollList){
+     innerHtml =`${innerHtml}
+     <tr>
+     <td><img class="profile" alt="" src="${empPayrollData._profilePic}">
+     </td>
+     <td>${empPayrollData._name}</td>
+     <td>${empPayrollData._gender}</td>
+     <td>${getDeptHtml(empPayrollData._department)}</td>
+     <td>${empPayrollData._salary}</td>
+     <td>${empPayrollData._startDate}</td>
+     <td>
+         <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+         <img name="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+     </td>
+ </tr>
+`;
+}
+document.querySelector('#table-display').innerHTML=innerHtml;
+}
+const getDeptHtml=(deptList)=>
+{
+  let deptHtml='';
+  for(const dept of deptList){
+     deptHtml=`${deptHtml}<div class='dept-label'>${dept}</div>`
+  }
+  return deptHtml;
+}
